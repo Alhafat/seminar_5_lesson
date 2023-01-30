2a6620818cdfa2cf5a715ac582dcde8b7b32bde5
Alhafat <vladimir-lukin@inbox.ru>
2023-01-30 22:38:35 +0300

    exp_2

---
+++ exp_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 32: Напишите программу замена элементов 
+// массива: положительные элементы замените на 
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.Clear();
+
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] array= new int[size];
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(minValue, maxValue+1);
+    }
+    return array;
+}
+
+System.Console.WriteLine("Введите количество элементов массива");
+int size=int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите минимальное значение элемента массива");
+int minValue=int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите максимальное значение элемента массива");
+int maxValue=int.Parse(Console.ReadLine()!);
+
+int[] numbers=GetArray(size, minValue, maxValue);
+Console.WriteLine(string.Join(", ", numbers));
+
+int[] newArray (int size)
+{
+    var array = numbers.ToArray();
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        array[i]*=-1;
+    }
+    return array;
+}
+
+numbers=newArray(size);
+Console.WriteLine(string.Join(", ", numbers));
